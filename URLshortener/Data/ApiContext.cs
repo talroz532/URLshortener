981310448c27c9c17ef14092ef13ea6ee743b187
talroz532 <67608539+talroz532@users.noreply.github.com>
2025-07-01 09:44:44 +0300

    Update ApiContext.cs
    comments

--- URLshortener/Data/ApiContext.cs
+++ URLshortener/Data/ApiContext.cs
@@ -1,16 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using URLshortener.Models;
 
 namespace URLshortener.Data
 {
+    // Database context for the application
     public class ApiContext : DbContext
     {
+        // Table for storing URL records
         public DbSet<Urllist> Urls { get; set; }
 
         public ApiContext(DbContextOptions<ApiContext> options)
-        :base(options)
+            : base(options)
         {
-        
         }
     }
 }
